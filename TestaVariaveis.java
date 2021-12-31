afeec99e87150908a8dc4e8afa31643ad93315e1
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2021-12-31 18:47:56 -0300

    Java JRE & JDK: first commit

---
+++ TestaVariaveis.java
@@ -0,0 +1,30 @@
+public class TestaVariaveis {
+
+    public static void main(String[] args) {
+
+        system.out.println("Hello new test!");
+
+        int age;
+        age = 44;
+        
+        System.out.println(age);
+
+        age = 22 + 22;
+
+        System.out.println(age);
+
+        age = (10 * 2) + 24;
+
+        System.out.println(age);
+
+        System.out.println("I have " + age + " year old");
+
+    }
+
+}
+
+/* Hello new test!
+44
+44
+44
+I have 44 year old */
---
+++ firstGreetings.java
@@ -0,0 +1,9 @@
+public class Program {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Devs! This is my first Java code... I'm so happy!");
+
+    }
+
+}
