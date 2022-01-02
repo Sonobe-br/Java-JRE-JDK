247c414baae8ea742caadc25dc5f1a3259db224c
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2022-01-02 18:17:06 -0300

    variáveis do tipo flutuante do Java. Os tipos vistos foram o (int) e (double) que usamos para operações aritméticas e concatenando com texto. Iniciamos as classes com letra maiúscula e as nossas funções e variáveis com letras minúsculas. Foi possível entender um pouco de type casting e como podemos passar um valor de um tipo para uma variável de outro.

---
+++ PontoFlutuante.java
@@ -0,0 +1,31 @@
+
+public class TestaPontoFlutuante {
+	
+	public static void main(String[] args) {
+		
+		double salary;
+		salary = 1500.70;
+		
+		System.out.println("My wage is US$ " + salary); 
+		
+		double division = 3.14 / 2;
+		
+		System.out.println(division);
+		
+		int otherDivision;
+		otherDivision = 5 / 2;
+		
+		System.out.println(otherDivision);
+		
+		double tryToShare;
+		tryToShare = 5.0 / 2;
+		System.out.println(tryToShare);
+		
+	}
+	
+}
+
+/* My wage is US$ 1500.7
+1.57
+2
+2.5 */
---
+++ TestaConversao.java
@@ -0,0 +1,17 @@
+public class TestaConversao {
+	
+	public static void main(String[] args) {
+		
+		double salary = 1500.70;
+		
+		int value = (int)salary;
+		
+		System.out.println(value);
+
+        //long bigNumber = 2687987741L; up to 64bits
+		//short smallValue = 7893; up to 16 bits
+		//byte b = 127; 
+
+	}
+
+}
