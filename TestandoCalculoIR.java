de339b4d6e0440aeb92de1e55daa997bef4899eb
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2022-01-03 19:13:25 -0300

    testando condicionais e calculo do IR a partir do salário.

---
+++ TestandoCalculoIR.java
@@ -0,0 +1,19 @@
+public class TestaIR {
+	
+	public static void main(String[] args) {
+		
+		double salario = 3300.00;
+		
+		if(salario < 2600.00) {
+			System.out.println("A sua aliquota é de 15%");
+			System.out.println("Você pode deduzir até R$ 350");
+		}
+		
+		if(salario < 3750.00) {
+			System.out.println("A sua aliquota é de 22,5%");
+			System.out.println("Você pode deduzir até R$ 636");
+		}
+		
+	}
+	
+}
