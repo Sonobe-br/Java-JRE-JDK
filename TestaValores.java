03c64ff83ca3e48e68c246eb2c36eee4479a5d6d
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2022-01-02 22:50:48 -0300

    testando valores

---
+++ TestaValores.java
@@ -0,0 +1,17 @@
+public class TestaValores {
+	
+	public static void main(String[] args) {
+		
+		int firstValue = 5;
+		
+		int secoundValue = 10;
+		
+		secoundValue = firstValue;
+		
+		firstValue = 20;
+		
+		System.out.println(secoundValue);
+		
+	}
+
+}
