9dc09e93a413be783d2f336238e16bca3dbbfa39
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2022-01-05 10:51:47 -0300

    atualizando informações

--- TestaIR.java
+++ TestaIR.java
@@ -1,7 +1,6 @@
-//Na faixa salarial entre R$ 1.434,00 e R$ 2.150,00 a alíquota será de 7,5%. 
-//Entre R$ 2.150,00 e R$ 2.866,00, 15%. 
-//Para trabalhadores com salários entre R$ 2.866,00 e R$ 3.582,00 o desconto será de 22,5%. 
-//A partir de R$ 3.582,00 permanece a alíquota atual de 27,5%.
+//De 1900.0 até 2800.0, o IR é de 7.5% e pode deduzir na declaração o valor de R$ 142.00
+//De 2800.01 até 3751.0, o IR é de 15% e pode deduzir R$ 350.00
+//De 3751.01 até 4664.00, o IR é de 22.5% e pode deduzir R$ 636.00
 
 public class TestaIR2 {
 		
