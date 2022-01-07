79b1a5efe948bd6256be33e688cf7d3f17c26b28
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2022-01-07 16:57:06 -0300

    laço com while, fixando o laço e escopo nos laços.

---
+++ TestWhile.java
@@ -0,0 +1,18 @@
+public class TestaWhile {
+	
+	public static void main(String[] args) {
+		
+		int counter = 0;
+		
+		while(counter <= 10) {
+			
+			System.out.println(counter);
+			
+			//counter = counter + 1;
+			//counter += 1;
+			counter ++;
+		}
+		System.out.println(counter);
+	}
+	
+}
